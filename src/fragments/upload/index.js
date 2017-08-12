663d81bf453b74aec1ea34494067b681e08a2ad6
Mahi22 <mahesh.masalkar22@gmail.com>
2017-08-12 21:49:51 +0530

    Added react-dropzone working

--- src/fragments/upload/index.js
+++ src/fragments/upload/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 import { Columns, Column } from 'src/grid';
 import { Card } from 'src/components/Card';
+import { Button } from 'src/elements';
 import './upload.scss';
 
-const Upload= () => (
-  <Card className="customCard">
-    <Columns isGapless className="upload">
-      <div className="leftContent ta_r">
-        <div className="plusIcon ta_c">
-          +
-        </div>
-      </div>
-      <div className="rightContent">
-        Right Upload
-      </div>
-    </Columns>
-  </Card>
-);
+class Upload extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onDrop(accepted, rejected) {
+    console.log(accepted);
+  }
+
+  render() {
+    let dropzoneRef = null;
+    return (
+      <Card className="customCard">
+        <Columns isGapless className="upload">
+          <Dropzone accept={"text/csv"} onDrop={this.onDrop.bind(this)} disableClick ref={(node) => { dropzoneRef = node; }} rejectClassName={"reject"}  className="leftContent ta_r">
+            <div className="plusIcon ta_c">
+              +
+            </div>
+            <Columns isCentered className="pv_2 content">
+              <Column>
+                <Button className="is-apollo button" onClick={() => { dropzoneRef.open() }}>Upload File</Button>
+              </Column>
+              <Column className="ta_l info">
+                OR DRAG AND DROP CSV HERE
+              </Column>
+            </Columns>
+          </Dropzone>
+          <div className="rightContent">
+            Right Upload
+          </div>
+        </Columns>
+      </Card>
+    );
+  }
+}
 
 export default Upload;
